afde05c9e8cea9f883a3005dea4ee863979aabc7
krudoppler <bkru33@gmail.com>
2024-10-08 15:56:09 -0400

    adding onclick logic

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-let buttonEl = document.getElementById("input-btn")
+const buttonEl = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 
-function consoleLog() {
-    console.log("Button Clicked!")
+
+buttonEl.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
