d150c17f1f7568a650bc5cb9732632a3bf443b82
krudoppler <bkru33@gmail.com>
2024-10-10 16:01:30 -0400

    Added css to the inputs

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const ulEl = document.getElementById("ul-el")
 let myLeads = []
 
 
-
+// On button click 1. Adds the value to myLeads array, 2. Clears the input field, 3. Initiates the renderLeads function
 buttonEl.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     renderLeads()
 })
 
+// 1. Defines empty string, 2. Executes For loop using string template to add LI and A tag from the array, 3. Renders listItems in the unorderlist ID
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     listItems += `
         <li>
             <a target='_blank' href='${myLeads[i]}'>
@@ -23,7 +22,6 @@ function renderLeads() {
             </a>
         </li>
     `
-
     console.log(listItems)
     }
     ulEl.innerHTML = listItems
