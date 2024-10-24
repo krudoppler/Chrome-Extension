bc771ea54fa1a8a83dd8242eb2b75b91144729c0
krudoppler <bkru33@gmail.com>
2024-10-24 19:07:07 -0400

    added delete button functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const buttonEl = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 let myLeads = []
 
 let localStorageLeads = JSON.parse(localStorage.getItem("myLeads"))
@@ -10,6 +11,12 @@ if (localStorageLeads) {
     myLeads = localStorageLeads
     renderLeads()
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads=[]
+    localStorage.clear()
+    renderLeads()
+})
 
 // On button click 1. Adds the value to myLeads array, 2. Clears the input field, 3. Initiates the renderLeads function
 buttonEl.addEventListener("click", function() {
